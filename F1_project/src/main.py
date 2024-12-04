3669449367e143946d113723f50446ae94609591
AndreLuiiZSantosS <104994626+AndreLuiiZSantosS@users.noreply.github.com>
2024-12-04 19:20:00 +0000

    Versão2

--- F1_project/src/main.py
+++ F1_project/src/main.py
@@ -1,11 +1,33 @@
 from src.menu_principal import exibir_menu_principal
+from src.menu_admin import exibir_menu_admin
 from src.banco_de_dados import carregar_dados, salvar_dados
+
+# Função de login
+def login():
+    print("=== Tela de Login ===")
+    username = input("Usuário: ")
+    password = input("Senha: ")
+
+    if username == "admin" and password == "admin":
+        return "admin"
+    else:
+        return "user"
 
 # Função principal que inicia o programa
 def main():
     dados = carregar_dados()
-    exibir_menu_principal(dados)
+
+    usuario = login()
+
+    if usuario == "admin":
+        exibir_menu_admin(dados)
+    else:
+        exibir_menu_principal(dados)
+
     salvar_dados(dados)
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except Exception as e:
+        print(f"Erro detectado: {e}")
